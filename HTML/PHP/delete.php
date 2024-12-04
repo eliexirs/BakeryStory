712e442161916a70e042964771b6b81a4404c3ab
Antonella <antonella.godoy.23@alumnos.uda.cl>
2024-12-04 03:55:39 -0300

    Se trabaja en el archivo delete.php para eliminar datos de la BD

---
+++ HTML/PHP/delete.php
@@ -0,0 +1,15 @@
+<?php
+include("conexion.php");
+
+$id_usuario=$_GET['id_usuario'];
+
+$sql="DELETE FROM usuario WHERE id_usuario = '$id_usuario'";
+$query=mysqli_query($conexion, $sql);
+
+if($query){
+    Header("Location: usuario.php");
+}
+
+mysqli_close($conexion);
+
+?>
